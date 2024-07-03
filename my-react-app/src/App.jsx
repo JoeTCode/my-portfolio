30f610a37c2e4c952dfb60c752aa69233b3203f0
JoeTCode <joejoe98t@gmail.com>
2024-07-03 20:54:16 +0100

    Hello, World!
    Initial React structure via Vite

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from './components/Nav.jsx';
+
+
+function App() {
+  return (
+    <Nav/>
+  );
+  
+}
+
+export default App
---
+++ my-react-app/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return (
+        <div>
+            Hello, World!
+        </div>
+    )
+}
+
+export default Nav;
