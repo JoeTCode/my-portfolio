e95fdc4dba3ad9dfbb55e5efeab1b521831c2dae
JoeTCode <joejoe98t@gmail.com>
2024-07-10 23:33:47 +0100

    Added white 3D text to hero model
    To do:
    - add sub section to hero text
    - possibly look into more css options for the 3d text?
    - Find suitable background image for whole page/ hero model?

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,22 +3,44 @@ import Contact from './components/Contact.jsx';
 import Hero from './components/Hero.jsx';
 import Projects from './components/Projects.jsx';
 
-import { Canvas} from '@react-three/fiber';
+import { Canvas, useLoader} from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { PerspectiveCamera } from '@react-three/drei';
 import Scene from '../public/Scene.jsx'; 
+import { extend } from '@react-three/fiber'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+extend({ TextGeometry })
 
 
+const Text = ({ text, color, ...props }) => {
+  const font = useLoader(FontLoader, '../public/fonts/Inter_Regular.json');
+  const textOptions = {
+    font,
+    size: 1,
+    depth: 0.1,
+  };
+
+  return (
+    <mesh {...props}>
+      <textGeometry args={[text, textOptions]} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+}
 
 const App = () => {
   return (
     <div>
         <Canvas>
-          <PerspectiveCamera makeDefault position={[5, 4.5, 12]} rotation={[0, 0, 0]} />
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <PerspectiveCamera makeDefault position={[-1, 4.5, 13]} rotation={[0, 0, 0]} />
+          <Text text="Hello, I'm Joe!" position={[-8, 4.5, 3]} color="white" />
           <OrbitControls
             enableZoom={false}
             minPolarAngle = {0} 
-            maxPolarAngle = {Math.PI / 3} 
+            maxPolarAngle = {Math.PI / 2.9} 
 
           />
           <Suspense fallback={null}>
