cfd1bf2008a6c5bf26c118a0914e8e4974d41ae4
JoeTCode <joejoe98t@gmail.com>
2024-07-10 23:01:23 +0100

    Restricted orbit controls of model
    Limited panning on Y-Axis, set starting viewpoint.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,16 +3,24 @@ import Contact from './components/Contact.jsx';
 import Hero from './components/Hero.jsx';
 import Projects from './components/Projects.jsx';
 
-import { Canvas } from '@react-three/fiber';
+import { Canvas} from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { PerspectiveCamera } from '@react-three/drei';
 import Scene from '../public/Scene.jsx'; 
+
 
 
 const App = () => {
   return (
     <div>
         <Canvas>
-          <OrbitControls/>
+          <PerspectiveCamera makeDefault position={[5, 4.5, 12]} rotation={[0, 0, 0]} />
+          <OrbitControls
+            enableZoom={false}
+            minPolarAngle = {0} 
+            maxPolarAngle = {Math.PI / 3} 
+
+          />
           <Suspense fallback={null}>
             <Scene />
           </Suspense>
