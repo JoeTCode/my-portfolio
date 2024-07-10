68d045a5c21c41f02e1be35c09276ca73f57b0cf
JoeTCode <joejoe98t@gmail.com>
2024-07-10 22:08:42 +0100

    Added 3d model
    Via Patrick Pan (YT)

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,22 @@
-
+import { Suspense } from 'react';
 import Contact from './components/Contact.jsx';
 import Hero from './components/Hero.jsx';
 import Projects from './components/Projects.jsx';
 
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Scene from '../public/Scene.jsx'; 
+
+
 const App = () => {
   return (
     <div>
-        
+        <Canvas>
+          <OrbitControls/>
+          <Suspense fallback={null}>
+            <Scene />
+          </Suspense>
+        </Canvas>
         <Hero/>
         <Projects/>
         <Contact/>
