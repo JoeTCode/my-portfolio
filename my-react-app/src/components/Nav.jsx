5723110237b7a6dd7b040bbfb61e60613b6aa80e
JoeTCode <joejoe98t@gmail.com>
2024-07-12 23:34:19 +0100

    Improved nav
    Added hiden nav functionality.
    The nav will be hidden unless:
    - The user scrolls
    - The user hovers over the nav area

--- my-react-app/src/components/Nav.jsx
+++ my-react-app/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, Events, scrollSpy, animateScroll as scroll } from 'react-scroll';
 
 
@@ -6,6 +6,8 @@ import { Link, Events, scrollSpy, animateScroll as scroll } from 'react-scroll';
     
 
 const Nav = () => {
+    const [showNavOnScroll, setShowNavOnScroll] = useState(false);
+
     // useEffect is used to perform side effects in functional components.
     // Here, it's used to register scroll events and update scrollSpy when the component mounts.
     useEffect(() => {
@@ -28,21 +30,66 @@ const Nav = () => {
         //   Events.scrollEvent.remove('begin');
         //   Events.scrollEvent.remove('end');
         // };
+
+        let timeoutId;
+        // Add scroll event listener to show nav on scroll
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+            setShowNavOnScroll(true);
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                setShowNavOnScroll(false);
+            }, 700); // time e.g: 100 = 100ms
+            } else {
+            setShowNavOnScroll(false);
+            }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        // Cleanup function to remove event listener when component unmounts
+        return () => {
+            clearTimeout(timeoutId);
+            window.removeEventListener('scroll', handleScroll);
+        };
+
     }, []);
 
+    const [hovered, setHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setHovered(true);
+      };
+    
+      const handleMouseLeave = () => {
+        setHovered(false);
+      };
+
     return (
-        <nav>
-            <div className="flex flex-col items-start z-10 fixed right-0 top-0 mt-10">
-                <Link activeClass="active" to="Hero" spy={true} smooth={true} duration={500} className="text-white p-2 mr-16 hover:cursor-pointer">
+        <nav className>
+            <div // this div detects when user mouses to nav, it's size determines detection radius
+            className="opacity-0 fixed right-[5rem] top-1 z-0 px-16 py-20 pb-[10rem]"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            >
+            Menu
+            </div>
+            <div className={`${
+            (hovered || showNavOnScroll) ? 'opacity-100' : 'opacity-0'
+            } transition-opacity duration-700 ease-in-out flex flex-col items-start z-10 fixed right-0 top-0 mt-1`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            >
+                <Link activeClass="active" to="Hero" spy={true} smooth={true} duration={500} className="nav-link">
                     Hero
                 </Link>
-                <Link activeClass="active" to="Introduction" spy={true} smooth={true} duration={500} className="text-white p-2 mr-16 hover:cursor-pointer">
+                <Link activeClass="active" to="Introduction" spy={true} smooth={true} duration={500} className="nav-link">
                     About Me
                 </Link>
-                <Link activeClass="active" to="Projects" spy={true} smooth={true} duration={500} className="text-white p-2 mr-16 hover:cursor-pointer">
+                <Link activeClass="active" to="Projects" spy={true} smooth={true} duration={500} className="nav-link">
                     Projects
                 </Link>
-                <Link activeClass="active" to="Contact" spy={true} smooth={true} duration={500} className="text-white p-2 mr-16 hover:cursor-pointer">
+                <Link activeClass="active" to="Contact" spy={true} smooth={true} duration={500} className="nav-link">
                     Contact
                 </Link>
             </div>
--- my-react-app/src/components/Projects.jsx
+++ my-react-app/src/components/Projects.jsx
@@ -1,17 +1,32 @@
 const Projects = () => {
     return (
-        <div className="grid gap-4 grid-cols-1 mx-[500px] pt-[150px] mt-[100px]">
+        <div className="grid gap-4 grid-cols-1 mx-[31rem] pt-[15rem]">
             <h2 className="text-4xl"> Projects: </h2>
             <div>
                 Project1
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum convallis ullamcorper augue id ornare. Proin at consequat nulla, ut feugiat sem. Aenean lacinia vitae ante eget dictum. Morbi enim nibh, lacinia eget luctus eu, vehicula eu mauris. Suspendisse nisi nunc, ultrices sit amet nunc ac, consequat elementum metus. Proin pretium a massa vel eleifend. Proin malesuada metus id justo ultrices ultrices. Nulla facilisi. Etiam vestibulum ac enim sed porta. Aliquam consequat pellentesque ex eu aliquam. Aliquam nibh libero, porttitor in metus at, luctus vestibulum odio. Cras pulvinar nulla at vulputate blandit.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum convallis ullamcorper augue id ornare sit amet nunc ac.
+                Proin at consequat nulla, ut feugiat sem. Aenean lacinia vitae ante eget
+                dictum. Morbi enim nibh, lacinia eget luctus eu, vehicula eu mauris.
+                Suspendisse nisi nunc, ultrices sit amet nunc ac, consequat elementum metus.
+                Proin pretium a massa vel eleifend. Proin malesuada metus id justo ultrices ultrices. 
+                Nulla facilisi. Etiam vestibulum ac enim sed porta. Aliquam consequat pellentesque ex
+                eu aliquam. Aliquam nibh libero, porttitor in metus at, luctus vestibulum odio. 
+                Cras pulvinar nulla at vulputate blandit. Ut feugiat sem.
 
 
             </div>
             <div>
                 Project2
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum convallis ullamcorper augue id ornare. Proin at consequat nulla, ut feugiat sem. Aenean lacinia vitae ante eget dictum. Morbi enim nibh, lacinia eget luctus eu, vehicula eu mauris. Suspendisse nisi nunc, ultrices sit amet nunc ac, consequat elementum metus. Proin pretium a massa vel eleifend. Proin malesuada metus id justo ultrices ultrices. Nulla facilisi. Etiam vestibulum ac enim sed porta. Aliquam consequat pellentesque ex eu aliquam. Aliquam nibh libero, porttitor in metus at, luctus vestibulum odio. Cras pulvinar nulla at vulputate blandit.
-
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum convallis ullamcorper augue id ornare.
+                Proin at consequat nulla, ut feugiat sem. Aenean lacinia vitae ante eget dictum.
+                Morbi enim nibh, lacinia eget luctus eu, vehicula eu mauris.
+                Suspendisse nisi nunc, ultrices sit amet nunc ac, consequat elementum metus.
+                Proin pretium a massa vel eleifend. Proin malesuada metus id justo ultrices ultrices. 
+                Nulla facilisi. Etiam vestibulum ac enim sed porta. Aliquam consequat pellentesque ex eu aliquam.
+                Aliquam nibh libero, porttitor in metus at, luctus vestibulum odio. 
+                Cras pulvinar nulla at vulputate blandit.
 
             </div>
         </div>
